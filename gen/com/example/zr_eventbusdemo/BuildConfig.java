9b76f2c356e509991a11f896d0f027e00eba405c
NetEase <netease@NetEasedeiMac-74.local>
2015-01-16 12:17:47 +0800

    init

---
+++ gen/com/example/zr_eventbusdemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.zr_eventbusdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/zr_eventbusdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.zr_eventbusdemo;
+
+
+import android.os.Bundle;  
+import android.support.v4.app.FragmentActivity;  
+  
+public class MainActivity extends FragmentActivity  
+{  
+    @Override  
+    protected void onCreate(Bundle savedInstanceState)  
+    {  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.activity_main);  
+    }  
+  
+}
